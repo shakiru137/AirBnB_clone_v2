a38bfbd55a6554ad087bba83bfad24e1699233f6
shakiru137 <yusufshakiruoluwasegun@gmail.com>
2024-04-30 00:48:37 -0700

    Updated

--- test.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-import sqlalchemy
-attr = (dir(sqlalchemy))
-string = 'create_engine'
-for arg in attr:
-    if string == arg:
-        print(f"{arg} is foung")
-    
-print("END")
