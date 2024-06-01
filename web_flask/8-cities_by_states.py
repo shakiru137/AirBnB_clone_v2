37a6375a565bb2f10ed393a635b21f3d7520555e
oluwasegun1379 <yusufshakiruoluwasegun137@gmail.com>
2024-06-01 06:26:24 +0100

    updated

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""Starts a Flask web application.
+"""
+Starts a Flask web application.
 
 The application listens on 0.0.0.0, port 5000.
 Routes:
@@ -14,7 +15,8 @@ app = Flask(__name__)
 
 @app.route("/cities_by_states", strict_slashes=False)
 def cities_by_states():
-    """Displays an HTML page with a list of all states and related cities.
+    """
+    Displays an HTML page with a list of all states and related cities.
 
     States/cities are sorted by name.
     """
